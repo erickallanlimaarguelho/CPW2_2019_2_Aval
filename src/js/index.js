d70d001dcd0a2b6ac4329847c543eb2fe48f42c5
erickallanarguelho <erickallan004@gmail.com>
2019-12-12 00:38:25 -0400

    versao inicial

---
+++ src/js/index.js
@@ -0,0 +1,206 @@
+var itens = [{ descricao: 'Escolha', unidade: 'Medida' },{ descricao: 'Abacaxi', unidade: 'UN' }, { descricao: 'Absorvente', unidade: 'PCT' },
+{ descricao: 'Achocolatado em pó', unidade: 'KG' }, { descricao: 'Açúcar', unidade: 'KG' },
+{ descricao: 'Água Sanitária', unidade: 'LITRO' }, { descricao: 'Alcaparras', unidade: 'UN' },
+{ descricao: 'Álcool em gel', unidade: 'UN' }, { descricao: 'Alface', unidade: 'MAÇO' },
+{ descricao: 'Alho', unidade: 'KG' }, { descricao: 'Arroz', unidade: 'KG' },
+{ descricao: 'Azeite de Olivia', unidade: 'UN' }, { descricao: 'Azeitona', unidade: 'KG' },
+{ descricao: 'Banana', unidade: 'KG' }, { descricao: 'Batata', unidade: 'KG' },
+{ descricao: 'Batata Palha', unidade: 'PCT' }, { descricao: 'Beterraba', unidade: 'KG' },
+{ descricao: 'Bolacha', unidade: 'PCT' }, { descricao: 'Café', unidade: 'KG' },
+{ descricao: 'Calabresa', unidade: 'KG' }, { descricao: 'Carne Bovína', unidade: 'KG' },
+{ descricao: 'Carne Suína', unidade: 'KG' }, { descricao: 'Carvão', unidade: 'kg' },
+{ descricao: 'Cebola', unidade: 'KG' }, { descricao: 'Cenoura', unidade: 'KG' },
+{ descricao: 'Cerveja', unidade: 'UN' }, { descricao: 'Chá', unidade: 'CX' },
+{ descricao: 'Cheiro-Verde', unidade: 'MAÇO' }, { descricao: 'Chocolate', unidade: 'KG' },
+{ descricao: 'Condicionador', unidade: 'UN' }, { descricao: 'Copo descartável', unidade: 'PCT' },
+{ descricao: 'Coxa sobre Coxa', unidade: 'KG' }, { descricao: 'Creme de leite', unidade: 'UN' },
+{ descricao: 'Creme dental', unidade: 'UN' }, { descricao: 'Iogurte', unidade: 'UN' },
+{ descricao: 'Desinfetante', unidade: 'LITRO' }, { descricao: 'Erva Tererá', unidade: 'KG' },
+{ descricao: 'Ervilha em conserva', unidade: 'KG' }, { descricao: 'Escova de dente', unidade: 'UN' },
+{ descricao: 'Extrato de Tomate', unidade: 'KG' }, { descricao: 'Farinha de Mandioca', unidade: 'KG' },
+{ descricao: 'Feijão', unidade: 'KG' }, { descricao: 'Fermento Quimico', unidade: 'UN' },
+{ descricao: 'Fio dental', unidade: 'UN' }, { descricao: 'Fósforo', unidade: 'CX' },
+{ descricao: 'Frango', unidade: 'KG' }, { descricao: 'Fubá', unidade: 'KG' },
+{ descricao: 'Gel de cabelo', unidade: 'UN' }, { descricao: 'Hot Pocket', unidade: 'UN' },
+{ descricao: 'Laranja', unidade: 'KG' }, { descricao: 'Leite', unidade: 'LITRO' },
+{ descricao: 'Leite Condensado', unidade: 'CX' }, { descricao: 'Lenha', unidade: 'KG' },
+{ descricao: 'Limão', unidade: 'KG' }, { descricao: 'Luva de latex', unidade: 'PCT' },
+{ descricao: 'Maçã', unidade: 'KG' }, { descricao: 'Macarrão', unidade: 'KG' },
+{ descricao: 'Macarrão instantaneo', unidade: 'UN' }, { descricao: 'Mamão', unidade: 'KG' },
+{ descricao: 'Manteiga', unidade: 'KG' }, { descricao: 'Margarina', unidade: 'KG' },
+{ descricao: 'Melancia', unidade: 'KG' }, { descricao: 'Melão', unidade: 'KG' },
+{ descricao: 'Milho verde em conserva', unidade: 'KG' }, { descricao: 'Mistura de bolo', unidade: 'PCT' },
+{ descricao: 'Molho de Pimenta', unidade: 'L' }, { descricao: 'Molho de Soja', unidade: 'L' },
+{ descricao: 'Mussarela', unidade: 'KG' }, { descricao: 'Óleo de soja', unidade: 'UN' },
+{ descricao: 'Ovos', unidade: 'DZ' }, { descricao: 'Pá de lixo', unidade: 'UN' },
+{ descricao: 'Palmito', unidade: 'KG' }, { descricao: 'Pano de chão', unidade: 'UN' },
+{ descricao: 'Pão', unidade: 'UN' }, { descricao: 'Papel Aluminio', unidade: 'UN' },
+{ descricao: 'Papel higiênico', unidade: 'PCT' }, { descricao: 'Papel Manteiga', unidade: 'UN' },
+{ descricao: 'Peixe', unidade: 'KG' }, { descricao: 'Pêra', unidade: 'KG' },
+{ descricao: 'Pimenta do Reino', unidade: 'G' }, { descricao: 'Pimentão', unidade: 'KG' },
+{ descricao: 'Milho de Pipoca', unidade: 'KG' }, { descricao: 'Plástico filme', unidade: 'UN' },
+{ descricao: 'Presunto', unidade: 'KG' }, { descricao: 'Rabanete', unidade: 'KG' },
+{ descricao: 'Refrigerente', unidade: 'UN' }, { descricao: 'Rodo', unidade: 'UN' },
+{ descricao: 'Rúcula', unidade: 'MAÇO' }, { descricao: 'Sabão em barra', unidade: 'KG' },
+{ descricao: 'Sabão em pó', unidade: 'KG' }, { descricao: 'Sabonete', unidade: 'UN' },
+{ descricao: 'Saco de lixo', unidade: 'PCT' }, { descricao: 'Sal', unidade: 'KG' },
+{ descricao: 'Shampoo', unidade: 'UN' }, { descricao: 'Steak de Frango', unidade: 'UN' },
+{ descricao: 'Tomate', unidade: 'KG' }, { descricao: 'Farinha de Trigo', unidade: 'KG' },
+{ descricao: 'Uva', unidade: 'KG' }, { descricao: 'Vassoura', unidade: 'UN' },
+{ descricao: 'Vinagre', unidade: 'UN' }, { descricao: 'Vinho', unidade: 'UN' }];
+
+
+function adicionar() {
+
+    for (let i = 0; i < itens.length; i++) {
+
+        let desc = itens[i].descricao;
+        let un = itens[i].unidade;
+
+        let lista = document.getElementById('opcoes');
+
+        let option = document.createElement('option');
+        option.text = itens[i].descricao;
+        option.value = i;
+        lista.appendChild(option);
+
+    }   
+
+}
+function aparecerUnidade(){
+    
+
+    let itenSelecionado = document.getElementById('opcoes').value;
+    let uniSelecionado = document.getElementById('UN');
+    uniSelecionado.innerText = itens[itenSelecionado].unidade;
+    let fakeSelecionado = document.getElementById('Fake');
+    fakeSelecionado.innerText = itens[itenSelecionado].descricao;
+    
+    
+}
+
+var vetLita = [];
+
+
+function salvarItem(){
+
+    /** Não deixa a pagina atualizar após esse evento */
+    event.preventDefault();
+    /** let nome = document.querySelector('#nome').nodeValue; */
+    let itenss = $ ('#Fake').text();
+    let un = $ ('#UN').text();
+    let quant = $ ('#qt').val();
+
+    let itemLista = new Item (itenss,un,quant);
+
+    vetLita.push(itemLista);
+
+    renderizarTabelaLista();
+    
+}
+
+function criaCabecalhoLista(){
+ 
+    /** Como minha lista vai funcionar como uma tabela, estou criando a tabela abaixo */
+    let cabecalho = document.createElement('thead');    /** Cria a primeira e principal linha da tabela */
+    let linhaCabecalho = document.createElement('tr');  /** Cria a minha primeira linha da tabela */
+    let usuario = document.createElement('th');
+    usuario.innerText = ' ';
+    let usuario1 = document.createElement('th');
+    usuario1.innerText = ' ';
+    let colunaItem = document.createElement('th');      /** Cria a coluna da tabela assim como as demais abaixo*/
+    colunaItem.innerText = 'Itens';                     /** Coloca um texto na coluna assim como as demais abaixo */
+    let colunaUni = document.createElement('th');
+    colunaUni.innerText = 'Unidade';
+    let colunaQuant = document.createElement('th');
+    colunaQuant.innerText = 'Quantidade';
+
+    /** Adiciona as colunas a linhaCabecalho */
+    linhaCabecalho.appendChild(usuario);
+    linhaCabecalho.appendChild(colunaItem);
+    linhaCabecalho.appendChild(colunaUni);
+    linhaCabecalho.appendChild(colunaQuant);
+    linhaCabecalho.appendChild(usuario1);
+    /** Adiciona a linhaCabecalho ao cabecalho */
+    cabecalho.appendChild(linhaCabecalho);
+
+    /** Essa função retorna apenas o cabeçalho da tebela */
+    return cabecalho;
+    
+}
+
+function criaCorpoLista(){
+
+    /**Cria o corpo da tabela */
+    let corpoTabela = document.createElement('tbody');
+
+    /** Verifica se há pelo menos um item na lista */
+    for(let i = 0; i < vetLita.length; i++){
+
+        let linha = document.createElement('tr');
+        
+        /** Cria as celular da tabela e adiciona elas a linha da tabela */
+        var img = document.createElement("img");
+        img.src = "src/img/carrinho.png"
+        linha.appendChild(img);
+
+        let celulaItens = document.createElement('td');
+        celulaItens.innerText = vetLita[i].itenss;
+        linha.appendChild(celulaItens);
+
+        let celulaUni = document.createElement('td');
+        celulaUni.innerText = vetLita[i].un;
+        linha.appendChild(celulaUni);
+
+        let celulaQt = document.createElement('td');
+        celulaQt.innerText = vetLita[i].quant;
+        linha.appendChild(celulaQt);
+
+        var img = document.createElement("img");
+        img.src = "src/img/excluir.png";
+        linha.appendChild(img);
+
+        /** Adiciona a linha ao corpo da tabela */
+        corpoTabela.appendChild(linha);
+
+    }
+
+    return corpoTabela;
+}
+
+function renderizarTabelaLista(){
+    if(vetLita.length > 0){
+        let areaListagemLista = document.getElementById('Lista');
+        areaListagemLista.innerHTML = '';
+        
+        let tabela = document.createElement('table');
+
+        let cabecalho = criaCabecalhoLista();
+        tabela.appendChild(cabecalho);
+
+        let corpoTabela = criaCorpoLista();
+        tabela.appendChild(corpoTabela);
+
+        areaListagemLista.appendChild(tabela);
+    }
+}
+
+class Item{
+
+    constructor(itenss, un, quant){
+        this.itenss = itenss;
+        this.un = un;
+        this.quant = quant;
+    }
+
+    
+}
+
+adicionar();
+
+
+
+
+
+
+
